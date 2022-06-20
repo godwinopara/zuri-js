c99991dd6a2fbe1ba4e8975f47d971b999734951
Godwin Opara <godwinopara62@yahoo.com>
2022-06-20 23:55:20 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,7 @@
+const name = "Godwin Opara";
+const height = 5'8;
+const country = "Nigeria";
+
+console.log(name)                // Godwin Opara
+console.log(height + " feet")     // 5'8 feet
+console.log(country)             // Nigeria
